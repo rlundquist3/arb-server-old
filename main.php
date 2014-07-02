0ebdb2f81aaa37b7b69c5a42a2d95dfe4cec2360
Riley Lundquist <rlundquist3@gmail.com>
2014-07-02 12:09:40 -0400

    added xml response for trail data

--- main.php
+++ main.php
@@ -25,12 +25,35 @@
 
 			echo $xml->asXML();
 		}
+
+		function trail_points() {
+			/*$query = $this->db->prepare('SELECT id, latitude, longitude FROM trail_points');
+			$query->execute();
+			$query->bind_result($id, $lat, $lon);
+
+			while ($query->fetch()) {
+				$point = new SimpleXMLElement('<rtept></rtept>');
+				$lat = $point->addChild('lat', $lat);
+				$lon = $point->addChild('lon', $lon);
+				echo $point->asXML();
+			}
+			$query->close();*/
+
+			$xml = new XMLReader();
+			if (!$xml->open('data/ArbTrails.xml')) {
+				die('Failed to open file');
+			}
+			while($xml->read()) {
+				echo $xml->readInnerXML();
+			}
+			$xml->close();
+		}
 	}
 
 	$api = new ArbAPI;
 
-	if ($_GET['type'] === 'trails')
-		$api->trails();
+	if ($_GET['type'] === 'trail_points')
+		$api->trail_points();
 	else if ($_GET['type'] === 'test')
 		$api->test();
 
